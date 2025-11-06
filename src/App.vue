a8d01e00f46988595e50b7af09a919292db91491
Ahsan Ali <64249060+AhsanAli24117@users.noreply.github.com>
2025-11-06 10:26:26 +0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import TaskForm from "./components/TaskForm.vue";
+
+const message = ref("Tasks App");
+
+const addTask = (newTask: string) => {
+  console.log(newTask);
+};
+</script>
+
+<template>
+  <main>
+    <h1>{{ message }}</h1>
+    <TaskForm @add-task="addTask" />
+  </main>
+</template>
+
+<style scoped>
+main {
+  width: 800px;
+  margin: 1rem auto;
+}
+</style>
---
+++ src/components/TaskForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <form @submit.prevent="formSubmitted">
+    <label>
+      New Task
+      <input v-model="newTask" type="text" name="newTask" />
+    </label>
+    <div class="button-container">
+      <button>Add</button>
+    </div>
+  </form>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const emit = defineEmits<{
+  addTask: [newTask: string];
+}>();
+
+const newTask = ref("");
+
+const formSubmitted = () => {
+  emit("addTask", newTask.value);
+  newTask.value = "";
+};
+</script>
+
+<style scoped>
+.button-container {
+  display: flex;
+  justify-content: end;
+}
+</style>
